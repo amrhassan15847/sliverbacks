c9c0179beb4c3bf84128b9df63b941a27cda91f2
amrhassan15847 <amrhassan15847@gmail.com>
2024-11-08 09:58:29 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,46 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: [
+        "intuitconsultancy.com",
+      "res.cloudinary.com",
+      "127.0.0.1",
+      "images.unsplash.com",
+      "ednuva.com",
+      "img.freepik.com",
+      "syaanh.com",
+      "www.lg.com",
+      "socialwb.com",
+      "developer.spotify.com",
+      "www.apple.com",
+      "image.winudf.com",
+      "encrypted-tbn0.gstatic.com",
+      "afripods-data.s3.amazonaws.com",
+      "encrypted-tbn3.gstatic.com",
+      "encrypted-tbn2.gstatic.com",
+      "encrypted-tbn0.gstatic.com",
+      "afripods-data.s3.amazonaws.com",
+      "www.helioninvestmentpartners.com",
+      "www.norrsken22.com",
+      "afripods-data.s3.amazonaws.com",
+      "www.helioninvestmentpartners.com",
+      "www.norrsken22.com",
+      "afripods-data.s3.amazonaws.com",
+      "www.helioninvestmentpartners.com",
+      "www.norrsken22.com",
+      "www.helioninvestmentpartners.com",
+      "www.norrsken22.com",
+      "placehold.co",
+      "img1.wsimg.com",
+      "african.business",
+      'forbesafrique.com'
+      ],
+    },
+    reactStrictMode: false,
+    // experimental: {
+    //   ppr: 'incremental',
+    // },
+  };
+  
+  export default nextConfig;
+  
